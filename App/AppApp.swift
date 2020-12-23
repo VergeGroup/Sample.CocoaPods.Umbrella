c2bc11e3259db092749a2a6ec86c7ec94533d143
Muukii <muukii.app@gmail.com>
2020-12-23 20:23:36 +0900

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Muukii on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
